9f910c894daf4adf5a56f6373ab259ab2fcd875d
Nermin Coric <ic22b080@technikum-wien.at>
2022-12-18 20:06:53 +0100

    Initial commit

---
+++ src/main/java/a/fhtw/uptimecalculator/UpTimeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package a.fhtw.uptimecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpTimeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpTimeCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/a/fhtw/uptimecalculator/UpTimeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package a.fhtw.uptimecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpTimeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
