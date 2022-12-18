0dae8059713516a9ee068e533ffeb85a07ee687b
Nermin Coric <ic22b080@technikum-wien.at>
2022-12-18 20:23:18 +0100

    add DownTimeController

---
+++ src/main/java/a/fhtw/uptimecalculator/controller/DownTimeController.java
@@ -0,0 +1,19 @@
+package a.fhtw.uptimecalculator.controller;
+
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.InvalidParameterException;
+
+public class DownTimeController {
+    @GetMapping("/downtime/minutes")
+    public double calculateDowntimeMinutes(@RequestParam double relative) {
+        if (relative < 0.0 || relative > 100.0)
+            throw new InvalidParameterException();
+        double minutesOfMonth = 60*24*30;
+        double defineUptimeInPercent = relative /100;
+        return minutesOfMonth * defineUptimeInPercent; //downtime in minutes
+    }
+
+}
